d9b7871e8d6822c1dc3af3bf8719517ca66e64e2
luiz-junior <junior.rodrigues0222@gmail.com>
2019-06-05 21:15:45 -0300

    finished

--- server.js
+++ server.js
@@ -8,13 +8,10 @@ const port = process.env.PORT || 8000;
 const userDb = process.env.USER_DB || 'admin';
 const passwordDB = process.env.PASSWORD_DB || 'admin123';
 
-app.get('/', (req, res) => {
-    res.send({ response: 'API Beleza na Web' });
-});
+app.get('/', (req, res) => res.send({ response: 'API Beleza na Web' }));
 
 app.use(express.json());
 app.use(cors());
-
 
 mongoose.connect(`mongodb://${userDb}:${passwordDB}@ds133137.mlab.com:33137/beleza_na_web`, { useNewUrlParser: true });
 requireDir('./src/models/');
